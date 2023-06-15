0f7a5c395a9be9a9dde374be111e1238953c8d6d
Rishabh Yadav <rishabhy461@gmail.com>
2023-06-15 22:27:18 +0530

    node project update

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
  
     const crypto = require("crypto");
+    let ejs = require('ejs');
     const fs = require("fs");
     const filebuffer = fs.readFileSync("./download.jpg");
+    const express = require('express');
 
     const hash = crypto.createHash("sha256");
    const finalHex = hash.update(filebuffer).digest('hex');
-
+ ejs.render('./index.ejs', {finalHex: finalHex})
    console.log(finalHex);
-
- 
+   var app = express();
+   app.use(express.static(__dirname + '/public'));
+   const PORT = 3000;
+    app.get('/', (req, res) => {
+    res.render('index.ejs', {finalHex: finalHex});
+    });
+   
+   app.listen(PORT, (error) =>{
+   if(!error){
+    
+   console.log("Server is Successfully Running,and App is listening on port "+ PORT)
+   }
+   else console.log("Error occurred, server can't start", error);
+});
